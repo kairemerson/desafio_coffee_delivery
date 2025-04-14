7a5a34eba2047994bf7106a6876470529d22b753
Remerson Monteiro <kairemerson@gmail.com>
2025-04-14 17:41:15 -0300

    feat: created component buttonquantity

--- src/components/ButtonQuantity/index.tsx
+++ src/components/ButtonQuantity/index.tsx
@@ -2,10 +2,16 @@ import { Minus, Plus } from "phosphor-react";
 import { Quantity } from "./styles";
 
 
-export function ButtonQuantity() {
+interface Props {
+    quantity: number
+    incrementQuantity: () => void
+    decrementQuantity: () => void
+}
+
+export function ButtonQuantity({quantity, incrementQuantity, decrementQuantity}: Props) {
     return(
         <Quantity>
-            <Minus size={14} weight="bold" /> <p>1</p> <Plus size={14}weight="bold" />
+            <Minus size={14} weight="bold" onClick={decrementQuantity}/> <p>{quantity}</p> <Plus size={14}weight="bold" onClick={incrementQuantity}/>
         </Quantity>
     )
 }
