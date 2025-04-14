ac56b59220839a09eae5d583cd43ff6078d3442e
Remerson Monteiro <kairemerson@gmail.com>
2025-04-14 17:47:05 -0300

    feat: created component success and add to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Home } from "./page/Home";
 import { Cart } from "./page/Cart";
+import { Success } from "./page/Success";
 
 
 export function Router() {
@@ -10,6 +11,7 @@ export function Router() {
             <Route path="/" element={<DefaultLayout/>}>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/cart" element={<Cart/>}/>
+                <Route path="/order/:orderId/success" element={<Success/>}/>
             </Route>
         </Routes>
     )
--- src/page/Success/index.tsx
+++ src/page/Success/index.tsx
@@ -2,10 +2,27 @@ import { InfoContent, InfoOrder, InfoSuccess, SuccessContainer } from "./styles"
 import delivery from "../../assets/delivery.svg"
 import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
 import { useTheme } from "styled-components";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartProvider";
+import { useParams } from "react-router-dom";
 
 export function Success() {
 
     const theme = useTheme()
+    const {orders} = useContext(CartContext)
+    const {orderId} = useParams()
+
+    const orderInfo = orders.find((order) => order.id === Number(orderId))
+    const paymentMethod = {
+        credit: 'Cartão de crédito',
+        debit: 'Cartão de débito',
+        cash: 'Dinheiro',
+    }
+
+    if (!orderInfo?.id) {
+        return null
+    }
+
     return (
         <SuccessContainer>
             <InfoSuccess>
@@ -16,8 +33,8 @@ export function Success() {
                     <div>
                         <MapPin size={32} weight="fill" style={{background: theme.purple}}/>
                         <div>
-                            <span>Entrega em <strong>Rua João Daniel Martinelli, 102</strong></span>
-                            <span>Farrapos - Porto Alegre, RS</span>
+                            <span>Entrega em <strong>{orderInfo?.rua}, {orderInfo?.numero}</strong></span>
+                            <span>{orderInfo?.bairro} - {orderInfo?.cidade}, {orderInfo?.uf}</span>
                         </div>
                     </div>
                     <div>
@@ -31,7 +48,7 @@ export function Success() {
                         <CurrencyDollar size={32} weight="fill" style={{background: theme.yellowDark}}/>
                         <div>
                             <span>Pagamento na entrega</span>
-                            <strong>Cartão de Crédito</strong>
+                            <strong>{paymentMethod[orderInfo.paymentMethod]}</strong>
                         </div>
                     </div>
                     
