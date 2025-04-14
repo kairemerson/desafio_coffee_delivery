8ae3392354ef5e8f6a1bbe06667a4e4abf100ec4
Remerson Monteiro <kairemerson@gmail.com>
2025-04-14 17:42:10 -0300

    add number of items to cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
-import { CartContainer, HeaderContainer } from "./styles";
+import { CartContainer, HeaderContainer, NumberItemsInCart } from "./styles";
 import logo from "../../assets/Logo.svg"
 import { NavLink } from "react-router-dom";
 import { MapPin, ShoppingCart } from "phosphor-react";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartProvider";
 
 export function Header() {
+
+    const {cart} = useContext(CartContext)
+
     return(
         <HeaderContainer>
             <img src={logo} />
             <nav>
                 <NavLink to="#"><MapPin size={22}  fill="#8047F8" weight="fill"/> <span>Porto Alegre, RS</span></NavLink>
-                <NavLink to="/checkout" title="checkout"><CartContainer><ShoppingCart size={22} fill="#DBAC2C" weight="fill"/></CartContainer></NavLink>
+                <NavLink to="/cart" title="checkout">
+                    <CartContainer><ShoppingCart size={22} fill="#C47F17" weight="fill"/></CartContainer>
+                    {cart.length > 0 && <NumberItemsInCart>{cart.length}</NumberItemsInCart>}
+                    
+                </NavLink>
             </nav>
         </HeaderContainer>
     )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -14,6 +14,7 @@ export const HeaderContainer = styled.header`
        
         height: 38px;
         a{
+            position: relative;
             text-decoration: none;
             display: flex;
             justify-content: center;
@@ -33,4 +34,19 @@ export const CartContainer = styled.div`
     padding: 0.5rem;
     background: ${props=>props.theme.yellowLight};
     height: 38px;
+`
+
+export const NumberItemsInCart = styled.span`
+    background: ${props => props.theme.yellowDark};
+    color: ${props => props.theme.white};
+    width: 1.25rem;
+    height: 1.25rem;
+    border-radius: 100%;
+    text-align: center;
+    padding: 0.2rem 0;
+    font-size: ${props => props.theme.textS};
+    font-weight: bold;
+    position: absolute;
+    top: -8px;
+    right: -8px;
 `
