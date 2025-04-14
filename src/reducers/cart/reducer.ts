e89642bb78701d35246950e76d6e3202cc397e3c
Remerson Monteiro <kairemerson@gmail.com>
2025-04-14 17:46:20 -0300

    feat: created reducers

---
+++ src/reducers/cart/reducer.ts
@@ -0,0 +1,73 @@
+import { newOrderData } from "../../page/Cart"
+import { ActionTypes } from "./actions"
+import {produce} from "immer"
+
+export interface Item {
+    id: string
+    quantity: number
+}
+
+export interface Order extends newOrderData {
+    id: number
+    items: Item[]
+}
+
+interface CartState {
+    cart: Item[]
+    orders: Order[]
+}
+
+export function cartReducer(state: CartState, action) {
+    switch(action.type) {
+        case ActionTypes.ADD_ITEM:
+            return produce(state, (draft)=>{
+                const itemAlready = draft.cart.find((item)=> item.id === action.payload.item.id)
+
+                if(itemAlready) {
+                    itemAlready.quantity += action.payload.item.quantity
+                }else{
+                    draft.cart.push(action.payload.item)
+                }
+            })
+        
+        case ActionTypes.REMOVE_ITEM:
+            return produce(state, (draft)=> {
+                const indexItemToRemove = draft.cart.findIndex((item)=> item.id === action.payload.itemId)
+
+                draft.cart.splice(indexItemToRemove, 1)
+            })
+
+        case ActionTypes.INCREMENT_ITEM_QUANTITY:
+            return produce(state, (draft)=> {
+                const itemToIncrement = draft.cart.find((item)=> item.id === action.payload.itemId)
+
+                if(itemToIncrement?.id) {
+                    itemToIncrement.quantity += 1
+                }
+            })
+
+        case ActionTypes.DECREMENT_ITEM_QUANTITY:
+            return produce(state, (draft)=> {
+                const itemToDecrement = draft.cart.find((item)=> item.id === action.payload.itemId)
+
+                if(itemToDecrement?.id && itemToDecrement.quantity > 1) {
+                    itemToDecrement.quantity -= 1
+                }
+            })
+
+        case ActionTypes.CHECKOUT_CART:
+            return produce(state, (draft)=> {
+                const newOrder = {
+                    id: new Date().getTime(),
+                    items: state.cart,
+                    ...action.payload.order,
+                }
+                draft.orders.push(newOrder)
+                draft.cart = []
+                action.payload.callback(`/order/${newOrder.id}/success`)
+            })
+        
+        default:
+            return state
+    }
+}
