bcc29fc2ab441bda2c36542c2022070db6394b13
Remerson Monteiro <kairemerson@gmail.com>
2026-01-14 16:24:19 -0300

    fix(types): correct typescript typing on reducer

--- src/reducers/cart/reducer.ts
+++ src/reducers/cart/reducer.ts
@@ -1,6 +1,21 @@
 import { newOrderData } from "../../page/Cart"
 import { ActionTypes } from "./actions"
 import {produce} from "immer"
+import {
+  addItemAction,
+  removeItemAction,
+  incrementItemQuantityAction,
+  decrementItemQuantityAction,
+  checoutCartAction,
+} from "./actions"
+import { NavigateFunction } from "react-router-dom"
+
+export type CartActions =
+  | ReturnType<typeof addItemAction>
+  | ReturnType<typeof removeItemAction>
+  | ReturnType<typeof incrementItemQuantityAction>
+  | ReturnType<typeof decrementItemQuantityAction>
+  | ReturnType<typeof checoutCartAction>
 
 export interface Item {
     id: string
@@ -17,98 +32,80 @@ interface CartState {
     orders: Order[]
 }
 
-interface AddItemAction {
-  type: ActionTypes.ADD_ITEM
-  payload: {
-    item: Item
-  }
-}
 
-interface RemoveItemAction {
-  type: ActionTypes.REMOVE_ITEM
-  payload: {
-    itemId: string
-  }
-}
-
-interface IncrementItemQuantityAction {
-  type: ActionTypes.INCREMENT_ITEM_QUANTITY
-  payload: {
-    itemId: string
-  }
-}
-
-interface DecrementItemQuantityAction {
-  type: ActionTypes.DECREMENT_ITEM_QUANTITY
-  payload: {
-    itemId: string
-  }
-}
-
-interface CheckoutCartAction {
-  type: ActionTypes.CHECKOUT_CART
-  payload: {
-    order: newOrderData
-    callback: (path: string) => void
-  }
-}
-
-export type CartActions =
-  | AddItemAction
-  | RemoveItemAction
-  | IncrementItemQuantityAction
-  | DecrementItemQuantityAction
-  | CheckoutCartAction
 
 export function cartReducer(state: CartState, action: CartActions) {
     switch(action.type) {
-        case ActionTypes.ADD_ITEM:
+        case ActionTypes.ADD_ITEM: {
+
+            const { item } = action.payload as { item: Item }
+
             return produce(state, (draft)=>{
-                const itemAlready = draft.cart.find((item)=> item.id === action.payload.item.id)
+                const itemAlready = draft.cart.find((item)=> item.id === item.id)
 
                 if(itemAlready) {
-                    itemAlready.quantity += action.payload.item.quantity
+                    itemAlready.quantity += item.quantity
                 }else{
-                    draft.cart.push(action.payload.item)
+                    draft.cart.push(item)
                 }
             })
-        
-        case ActionTypes.REMOVE_ITEM:
+        }
+
+        case ActionTypes.REMOVE_ITEM:{
+
+            const { itemId } = action.payload as { itemId: string }
+
             return produce(state, (draft)=> {
-                const indexItemToRemove = draft.cart.findIndex((item)=> item.id === action.payload.itemId)
+                const indexItemToRemove = draft.cart.findIndex((item)=> item.id === itemId)
 
                 draft.cart.splice(indexItemToRemove, 1)
             })
+        }
 
-        case ActionTypes.INCREMENT_ITEM_QUANTITY:
+        case ActionTypes.INCREMENT_ITEM_QUANTITY:{
+            const { itemId } = action.payload as { itemId: string }
+
             return produce(state, (draft)=> {
-                const itemToIncrement = draft.cart.find((item)=> item.id === action.payload.itemId)
+                const itemToIncrement = draft.cart.find((item)=> item.id === itemId)
 
                 if(itemToIncrement?.id) {
                     itemToIncrement.quantity += 1
                 }
             })
+        }
 
-        case ActionTypes.DECREMENT_ITEM_QUANTITY:
+        case ActionTypes.DECREMENT_ITEM_QUANTITY:{
+
+            const { itemId } = action.payload as { itemId: string }
+
             return produce(state, (draft)=> {
-                const itemToDecrement = draft.cart.find((item)=> item.id === action.payload.itemId)
+                const itemToDecrement = draft.cart.find((item)=> item.id === itemId)
 
                 if(itemToDecrement?.id && itemToDecrement.quantity > 1) {
                     itemToDecrement.quantity -= 1
                 }
             })
+        }
 
-        case ActionTypes.CHECKOUT_CART:
-            return produce(state, (draft)=> {
+        case ActionTypes.CHECKOUT_CART: {
+            const { order, callback } = action.payload as {
+                order: newOrderData
+                callback: NavigateFunction
+            }
+
+            return produce(state, (draft) => {
                 const newOrder = {
-                    id: new Date().getTime(),
-                    items: state.cart,
-                    ...action.payload.order,
+                id: new Date().getTime(),
+                items: state.cart,
+                ...order,
                 }
+
                 draft.orders.push(newOrder)
                 draft.cart = []
-                action.payload.callback(`/order/${newOrder.id}/success`)
+                callback(`/order/${newOrder.id}/success`)
             })
+        }
+
         
         default:
             return state
